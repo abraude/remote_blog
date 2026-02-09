746e5bd0eb6d1aa2470943d10f35fc5aa06b8879
ABB <abb@fkbp.org>
2026-02-09 18:24:48 +0100

    shadcn-svelte installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,7 @@
 		</p>
 		<p class="text-gray-600">
 			Visit
-			<a
-				href="https://svelte.dev/docs/kit"
-				class="text-blue-600 underline hover:text-blue-800"
+			<a href="https://svelte.dev/docs/kit" class="text-blue-600 underline hover:text-blue-800"
 				>svelte.dev/docs/kit</a
 			>
 			to read the documentation
